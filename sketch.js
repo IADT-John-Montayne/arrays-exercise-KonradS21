cefdf98d7633c595f8e2f3a752020fcae9fc6e64
KonradS21 <N00243433@student.iadt.ie>
2026-01-20 12:21:53 +0000

    recap on arrays

--- sketch.js
+++ sketch.js
@@ -1,3 +1,26 @@
+let names = ["Peter", "Meg", "Lois"]; // Array of names
+console.log(names); //calling the names
+console.log(names.length); //checking the length of the array
+
+names[1]="Brian"; //changing the second name (Meg) to (Brian)
+names.push("Stewie"); //adding a new name (Stewie) to the array
+names.push("Chris");
+console.log(names);
+console.log(names.length); //checking the length of the array
+
+names.pop(); //removing the last name (Chris) from the array
+console.log(names);
+
+names.unshift("Cleveland"); //adding a new name (Cleveland) to the beginning of the array
+console.log(names);
+
+names.shift(); //removing the first name (Cleveland) from the array
+console.log(names);
+
+for(let i = 0; i < names.length; i++) { //looping through the array to print each name
+	console.log(names[i]);
+}
+
 function setup() {
 	createCanvas(500, 500);
 	background(55);
